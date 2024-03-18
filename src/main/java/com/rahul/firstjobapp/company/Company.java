b3d2d729c446eeb6c32143686f59762adc8455af
Rahul Deo <inform.rahuldeo@gmail.com>
2024-03-18 23:30:48 +0530

    first job app

---
+++ src/main/java/com/rahul/firstjobapp/company/Company.java
@@ -0,0 +1,82 @@
+package com.rahul.firstjobapp.company;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.rahul.firstjobapp.job.Job;
+import com.rahul.firstjobapp.review.Review;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Company {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String description;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "company")
+	private List<Job> jobs;
+	
+	@OneToMany(mappedBy = "company")
+	private List<Review> reviews;
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+	public Company() {
+	}
+
+	public Company(Long id, String name, String description, List<Job> jobs) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.jobs = jobs;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Job> getJobs() {
+		return jobs;
+	}
+
+	public void setJobs(List<Job> jobs) {
+		this.jobs = jobs;
+	}
+	
+	
+}
